f4574aee4066d56bcb97a2ea69f83a7b3e725499
Kartik-Rao11 <kartik.rao29@gmail.com>
2022-03-10 22:40:48 +0530

    -s command complete

--- wcat.js
+++ wcat.js
@@ -6,6 +6,7 @@
 // node wcat.js f1.txt
 //node wcat.js f1.txt f2.txt f3.txt
 
+const { Console } = require("console");
 const fs= require("fs");
 
 //to take input and we use slice because we dont require the extra information
@@ -14,11 +15,21 @@ let inputArr= process.argv.slice(2);
 //console.log(inputArr);
 
 let filesArr=[];
+let optionsArr = [];
 
-//to take files input into different array
-for(let i=0; i<inputArr.length; i++){
-    filesArr.push(inputArr[i]);
+//===============> placed files path in filesArr <=============
+for (let i = 0; i < inputArr.length; i++){
+    let firstChar = inputArr[i].charAt(0);
+    // console.log(firstChar);
+    if (firstChar == '-') {
+        optionsArr.push(inputArr[i]);
+    }
+    else {
+        filesArr.push(inputArr[i]);
+    }
 }
+
+
 
 //to check if all the files are present or not
 for(let i=0; i<filesArr.length; i++){
@@ -34,7 +45,32 @@ let content="";
 
 for(let i=0; i<filesArr.length; i++){
     let fileContent= fs.readFileSync(filesArr[i]);
-    content+= fileContent+"\n";
+    content+= fileContent+"\r\n";
 }
 
 console.log(content);
+
+let contentArr = content.split("\r\n");
+console.table(contentArr);
+
+// now for command -s (to remove extra space)
+let isSPresent = optionsArr.includes("-s");
+if (isSPresent) {
+    for (let i = 1; i < contentArr.length; i++){
+        if (contentArr[i] == "" && contentArr[i - 1] == "") {
+            contentArr[i] = null;
+        }
+        else if (contentArr[i] == "" && contentArr[i - 1] == null) {
+            contentArr[i] = null;
+        }
+    }
+    console.table(contentArr);
+    let tempArr = [];
+    //push everything in tempArr except null
+    for (let i = 0; i < contentArr.length; i++){
+        if (contentArr[i] != null) {
+            tempArr.push(contentArr[i]);
+        }
+    }
+    console.log("data after removing extra lines\n",tempArr);
+}
